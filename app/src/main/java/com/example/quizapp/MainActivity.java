8ff9d12f61305a2b44e5c561292e537424970d71
QuangVip2441 <nguyenquang124042k1@gmail.com>
2024-04-10 01:15:08 +0700

    insert login activity and update home fragment

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.example.quizapp.Models.QuestionModel;
+import com.example.quizapp.Views.HomeFragment;
+import com.example.quizapp.ultils.FragmentUtils;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<QuestionModel> mQuestions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mQuestions = new ArrayList<>();
+
+        replaceFragment(new HomeFragment());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentUtils.replaceFragment(
+                getSupportFragmentManager(),
+                fragment,
+                false);
     }
 }
--- app/src/main/java/com/example/quizapp/Views/HomeFragment.java
+++ app/src/main/java/com/example/quizapp/Views/HomeFragment.java
@@ -7,17 +7,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.quizapp.R;
 
 public class HomeFragment extends Fragment {
 
-
+    private TextView hiddenContent, txtNameApp;
+    private ImageButton btnToggle;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btnToggle = view.findViewById(R.id.btnToggle);
+        hiddenContent = view.findViewById(R.id.hiddenContent);
+        txtNameApp = view.findViewById(R.id.txtNameApp);
+
+        btnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleContent();
+            }
+        });
         return view;
+    }
+    private void toggleContent() {
+        if (hiddenContent.getVisibility() == View.VISIBLE) {
+            hiddenContent.setVisibility(View.GONE);
+            btnToggle.setImageResource(R.drawable.ic_show);
+        } else {
+            hiddenContent.setVisibility(View.VISIBLE);
+            btnToggle.setImageResource(R.drawable.ic_hide);
+        }
     }
 }
